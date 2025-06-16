4f191de668724f4ea980ecbfd90fc943760d3a18
241024 <241024@std.hi-joho.ac.jp>
2025-06-16 12:28:48 +0900

    うｐ

--- src/kadai1/Character.java
+++
@@ -1,9 +0,0 @@
-package kadai1;
-public abstract class Character {
-    String name;
-    int hp;
-    public void run() {
-        System.out.println(this.name + "は逃げ出した！");
-    }
-    public abstract void attack(Matango m);
-}
